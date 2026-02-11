d3a5091cb7cd72c6b52821bda0a4f02239c72aae
yinghau68 <yinghautsai68cycu@gmail.com>
2026-02-11 09:09:30 +0800

    change base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bookmark/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
